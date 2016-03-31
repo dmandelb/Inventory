f613eae485aa16fc0994817dd8a9e20cac1df045
dmandelb <devin.mandelbaum@gmail.com>
2016-03-31 14:45:57 -0400

    Write out migrations

--- db/migrate/20160331182944_create_categories.rb
+++ db/migrate/20160331182944_create_categories.rb
@@ -1,6 +1,10 @@
 class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
+      t.string :name, null: false
+      t.integer :list_id, null: false
+
+      t.timestamps null: false
     end
   end
 end
--- db/migrate/20160331183006_create_items.rb
+++ db/migrate/20160331183006_create_items.rb
@@ -1,6 +1,10 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
+      t.string :name, null: false
+      t.integer :subcategory_id, null: false
+
+      t.timestamps null: false
     end
   end
 end
