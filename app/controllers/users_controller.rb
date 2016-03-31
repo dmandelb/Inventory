8972394a75d16fbd145cf70940364cfa6a34f7d4
dmandelb <devin.mandelbaum@gmail.com>
2016-03-31 17:28:08 -0400

    Complete authentication and add login/logout/signup links to application layout page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,21 @@
 class UsersController < ApplicationController
+  skip_before_action :ensure_login, only: [:new, :create]
+  def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.new(user_params)
+    if @user.authenticate(params[:user][:password_confirmation]) && @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
